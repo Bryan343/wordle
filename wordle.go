697f35a53b9124d10c6957d0bd3c834095f871c3
Bryan343 <bryanlml343@gmail.com>
2024-11-30 22:30:58 -0500

    completed main logic, trying to implement UI, info and unlimited mode :)

--- wordle.go
+++ wordle.go
@@ -3,37 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"golang.org/x/text/runes"
-	"golang.org/x/text/transform"
-	"golang.org/x/text/unicode/norm"
 	"log"
 	"math/rand"
 	"os"
-	"unicode"
+	"strings"
 	"unicode/utf8"
 )
 
 var answers []string = getWordsFromTxt()
+var gameWords []string
 
-func main() {
-	word := answers[rand.Intn(len(answers) - 1)]
-	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
-	wordWithoutAccents, _, _ := transform.String(t, word)
-	fmt.Println("ANSWER: ", word)
-	fmt.Println("ANSWER WITHOUT ACCENTS: ", wordWithoutAccents)
-	score := 0
-
-	if playRound(wordWithoutAccents) == true {
-		fmt.Println("Correct")
-		score++
-	} else {
-		fmt.Println("ANSWER: ", word)
-		score = 0
-	}
-}
-
+// Get all the words that contain 5 letters
 func getWordsFromTxt() []string {
-	var wordsTxt []string
+	wordsTxt := []string{}
 	file, err := os.Open("./res/spanish_words.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -41,10 +23,9 @@ func getWordsFromTxt() []string {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		test := scanner.Text()
-		if utf8.RuneCountInString(test) == 5 {
-			wordsTxt = append(wordsTxt, test)
-			fmt.Println(test)	
+		word := scanner.Text()
+		if utf8.RuneCountInString(word) == 5 {
+			wordsTxt = append(wordsTxt, word)
 		}
 	}
 	file.Close()
@@ -52,23 +33,97 @@ func getWordsFromTxt() []string {
 	return wordsTxt
 }
 
-func playRound(roundWord string) bool {
-	var answer string 
-	var answers []string
+func main() {
+	playGame()
+}
 
+// Play a full game (with rounds)
+func playGame() {
+	answer := answers[rand.Intn(len(answers)-1)]
+	score := 0
+
+	if playRound(answer) == true {
+		fmt.Println("Correct")
+		score++
+	} else {
+		fmt.Println("¡Has perdido! La palabra era:\033[38;5;41m", answer, "\033[0m")
+		score = 0
+	}
+}
+
+// Play a round
+func playRound(answer string) bool {
+	guess := ""
+	guessedWords := []string{}
+	//guessedLetters := []string{}
 	fmt.Println("Escribe una palabra de cinco letras")
-	for len(answers) <= 5  {
-		fmt.Scan(&answer)
-		if utf8.RuneCountInString(answer) != 5 {
+	for len(guessedWords) <= 5 {
+		//fmt.Println("Escribe !info para mostrar la información de la ronda acutal")
+		fmt.Scan(&guess)
+		/*if guess == "!info" {
+			showRoundInfo(guessedLetters)
+			continue
+		}*/
+		if utf8.RuneCountInString(guess) != 5 {
 			fmt.Println("Escribe una palabra de cinco letras")
 			continue
 		}
 
-		if answer == roundWord {
+		if guess == answer {
 			return true
 		}
 
-		answers = append(answers, answer)
+		guessChars := []rune(guess)
+		coloredWord := strings.Builder{}
+		for i, r := range guessChars {
+			containsLetter, samePos := compareChars(answer, r, i)
+			coloredLetter := strings.Builder{}
+			if containsLetter {
+				if samePos {
+					coloredLetter.WriteString("\033[38;5;46m")
+				} else {
+					coloredLetter.WriteString("\033[38;5;226m")
+				}
+			} else {
+				coloredLetter.WriteString("\033[38;5;238m")
+			}
+			coloredLetter.WriteRune(r)
+			coloredLetter.WriteString("\033[0m")
+			//guessedLetters = append(guessedLetters, coloredLetter.String())
+			coloredWord.WriteString(coloredLetter.String())
+		}
+		fmt.Println(coloredWord.String())
+
+		guessedWords = append(guessedWords, coloredWord.String())
 	}
 	return false
 }
+
+// Check if the string contains the letter and if it is in the same positiion
+func compareChars(answer string, letter rune, i int) (bool, bool) {
+	var (
+		contains    bool
+		samePos     bool
+		answerChars = []rune(answer)
+	)
+	for ai, ac := range answerChars {
+		if ac == letter {
+			contains = true
+			if ai == i {
+				samePos = true
+			}
+		}
+	}
+
+	return contains, samePos
+}
+
+// Show round information (letter found in the word,
+// letters that are not in the word and letters that haven-t been entered
+/*func showRoundInfo(guessedLetters []string) {
+	validLetters := []string{}
+	invalidLetters := []string{}
+	for i, guess := range guessedLetters {
+
+	}
+}*/
